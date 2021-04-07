bd64ee4d23cda5c9c3d175f2dc28b0fc60694602
alpha-bot-discord <78361574+alpha-bot-discord@users.noreply.github.com>
2021-04-07 15:07:59 +0300

    Update pvp.lua

--- pvp.lua
+++ pvp.lua
@@ -1,4 +1,12 @@
-AddEventHandler("playerSpawned", function(spawn)
-	SetCanAttackFriendly(GetPlayerPed(-1), true, false)
-	NetworkSetFriendlyFireOption(true)
+AddEventHandler("playerSpawned", function()
+  Citizen.CreateThread(function()
+
+    local player = PlayerId()
+    local playerPed = GetPlayerPed(-1)
+
+    -- Enable pvp
+    NetworkSetFriendlyFireOption(true)
+    SetCanAttackFriendly(playerPed, true, true)
+
+  end)
 end)
